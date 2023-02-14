9f4eb3d734b01033372467472daf28c1082c0ef9
DmitryRyzhik <rdssdr03@gmail.com>
2023-02-14 18:53:01 +0800

    Домашнее задание к семинару 2

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>99 && num<1000)
+{
+    int x=(num%100)/10;
+    System.Console.WriteLine(x + " ");
+}
+else 
+{
+    System.Console.WriteLine("чичсло НЕ трехзначное");
+}
